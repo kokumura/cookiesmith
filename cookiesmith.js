d5e71441de9d93eb0997403e663ecdfc3bd8b711
Kosuke Okumura <rentan7kg@gmail.com>
2013-09-21 18:18:38 +0900

    fix log format prevent double load append version number to bookmarklet

--- cookiesmith.js
+++ cookiesmith.js
@@ -1,4 +1,7 @@
 var Cookiesmith = (function($g,$app){
+  if(Cookiesmith!==undefined){
+    return CookieSmith;
+  }
   var $o = $g.ObjectsById;
   var $u = $g.UpgradesById;
   /*
@@ -43,7 +46,10 @@ var Cookiesmith = (function($g,$app){
   var GoldHunter = $app.GoldHunter = {};
   GoldHunter.hunt = function(){
     if($g.goldenCookie.delay==0){
-      window.setTimeout(function(){$g.goldenCookie.click()},1000);
+      window.setTimeout(function(){
+        $g.goldenCookie.click();
+        console.log('got Golden Cookie!');
+      },1000);
     }
   };
   GoldHunter.start = function(){
@@ -174,7 +180,7 @@ var Cookiesmith = (function($g,$app){
           scores.push( this.func.score($o[i],this.baseTime) );
         }
         var maxs = this.func.maxBy( scores, function(s){return s.s} );
-        console.log( this.func.map( scores, function(o){ return o.obj.price/o.obj.cps() } ) );
+        //console.log( this.func.map( scores, function(o){ return o.obj.price/o.obj.cps() } ) );
         target.obj = maxs.obj;        
       }
     }
@@ -186,11 +192,11 @@ var Cookiesmith = (function($g,$app){
 
     if(target.obj){
       var delay = target.obj.price<$g.cookies ? 0 : (target.obj.price-$g.cookies)/$g.cookiesPs;
-      console.log('planning to buy '+target.obj.name+' at '+Beautify(target.obj.price)+' delayed '+Math.round(delay)+' seconds' );
+      console.log('plan to buy '+target.obj.name+' at '+Beautify(target.obj.price)+' within '+Math.round(delay)+' seconds' );
 
     } else if (target.ug){
       var delay = target.ug.basePrice<$g.cookies ? 0 : (target.ug.basePrice-$g.cookies)/$g.cookiesPs;
-      console.log('planning to unlock '+target.ug.name+' at '+Beautify(target.ug.basePrice)+' delayed '+Math.round(delay)+' seconds' );
+      console.log('plan to buy '+target.ug.name+' at '+Beautify(target.ug.basePrice)+' within '+Math.round(delay)+' seconds' );
 
     }
 
@@ -199,6 +205,8 @@ var Cookiesmith = (function($g,$app){
   SimpleBuyer.prototype.func.score = function(obj,baseTime){
     var cpcps = obj.price / obj.cps();
     var delay = obj.price > $g.cookies ? (obj.price-$g.cookies)/$g.cookiesPs : 0;
+
+    // 待ち時間のコストは指数関数で上がるようにしたみた。
     return {
       s: - ( cpcps * ( Math.pow(2,delay/30) ) ),
       obj: obj,
@@ -209,7 +217,16 @@ var Cookiesmith = (function($g,$app){
     for(var i=0;i<$u.length;i++){
       var ug = $u[i];
       if(ug.bought===1 || ug.unlocked===0 ){ continue; }
-      if( ug.basePrice * 1.5 < maxPriceObj.price ){
+
+      // このへん適当なので何とかしたい
+      if(maxPriceObj.price < 800000){
+        var r = 1.5;
+      } else if(maxPriceObj.price < 1666666) {
+        var r = 1.0;
+      } else {
+        var r = 1/1.5;
+      }
+      if( ug.basePrice < maxPriceObj.price * r ){
         return ug;
       }
     }
@@ -230,17 +247,21 @@ var Cookiesmith = (function($g,$app){
   /*
    * initializer
    */
+  var initialized = false;
   $app.init = function(){
-    console.log('cookiesmith: initialize');
+    if(initialized) return $app;
     Interceptor.set();
-    $app.init = function(){return $app}
+    initialized = true;
     return $app;
   };
+  var started = false;
   $app.autoStart = function(){
+    if(started) return $app;
     $app.init();
     $app.Clicker.start();
     $app.GoldHunter.start();
     $app.Buyer.start();
+    started = true;
     return $app;
   };
   return $app;
