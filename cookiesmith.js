5b5883d5bd9e29491173c11564800025a39ec755
Kosuke Okumura <rentan7kg@gmail.com>
2013-09-23 05:46:29 +0900

    improved Upgrade logic

--- cookiesmith.js
+++ cookiesmith.js
@@ -4,7 +4,9 @@ var Cookiesmith = (function($g,$app){
     Cookiesmith.remove();
   }
   var $o = $g.ObjectsById;
+  var $O = $g.Objects;
   var $u = $g.UpgradesById;
+  var $U = $g.Upgrades;
 
   /*
    * Utility
@@ -48,7 +50,9 @@ var Cookiesmith = (function($g,$app){
   };
   Util.pad0 = function(value,digit){
     if( Math.log(value)/Math.log(10) < digit ){
-      return ( value * Math.pow(0.1,digit)).toString().substring(2,2+digit);
+      var base = '0';
+      for(var i=2;i<digit;i++) base+='0';
+      return (base+value).slice(-digit);
     } else {
       return Math.round(value.toString());
     }
@@ -173,25 +177,30 @@ var Cookiesmith = (function($g,$app){
     var itv = $g.time-this.last.time;
     var clicks = $g.cookieClicks - this.last.cookieClicks;
     var clickCps = itv===0 ? 0 : $g.computedMouseCps * clicks / (itv/1000);
+    this.clicksPs = itv===0 ? 0 : clicks / (itv/1000);
+    this.clickCps = clickCps;
     this.realCps = $g.cookiesPs + clickCps;
 
     this.action();
-    
-    this.saveStatus();
-    if(this.nextTime <= $g.T){
-      this.nextTime += this.interval;
+
+    if (this.last.time+this.interval < $g.time){
+      this.saveStatus();
+    }
+    if(this.nextTime <= $g.time){
+      this.nextTime = $g.time + this.interval;
     }
   };
   BasicBuyer.prototype.start = function(){
     var self = this;
     this.init();
+    this.nextTime = $g.time + this.interval;
     Interceptor.loopHook[this.interceptorKey] = function(){self.loop()};
   };
   BasicBuyer.prototype.stop = function(){
     delete Interceptor.loopHook[this.interceptorKey];
   };
   BasicBuyer.prototype.action = function(){
-    this.nextTime = $g.T + this.interval;
+    this.nextTime = $g.time + this.interval;
   };
 
   /*
@@ -204,7 +213,12 @@ var Cookiesmith = (function($g,$app){
     BasicBuyer.prototype.init.apply(this); // super()
     this.interval = 1000;
     this.interceptorKey = 'simpleBuyer';
-    this.costParam = 60;
+    this.param = {
+      costDenom: 60,
+      luckyCookiesThreshold: 90,
+      upgradeDefaultThreshold: 60,
+    };
+    this.policiesForUpgrade = this.getPoliciesForUpgrade();
     this.action = this.choose;
   }
   SimpleBuyer.prototype.buy = function(){
@@ -214,7 +228,7 @@ var Cookiesmith = (function($g,$app){
       return this.choose();
     }
 
-    if( this.choice.obj ){
+    if(this.choice.type==='obj'){
       var obj = this.choice.obj;
       if(obj.price > $g.cookies){ return; }
       this.choice = undefined;
@@ -223,7 +237,7 @@ var Cookiesmith = (function($g,$app){
       obj.buy();
       Util.log('bought '+(obj.bought===1? 'the first ' : 'a ')+obj.name+' at '+Beautify(price) );
 
-    } else if (this.choice.ug) {
+    } else if (this.choice.type==='ug') {
       var ug = this.choice.ug;
       if(ug.basePrice > $g.cookies){ return; }
       this.choice = undefined;
@@ -235,24 +249,17 @@ var Cookiesmith = (function($g,$app){
   };
 
   SimpleBuyer.prototype.choose = function(){
-    var target = {};
-    if($g.coookiesPs<0.1){
-      target.obj = $o[0];
-    } else {
-
-      var ug = this.considerUpgrade();
-      if(ug!==undefined){
-        target.ug = ug;
-
-      } else {
-        var scores = [];
-        for(var i=0;i<$o.length;i++){
-          scores.push( this.score($o[i],this.baseTime) );
-        }
-        var maxs = Util.maxBy( scores, function(s){return s.s} );
-        //console.log( Util.map( scores, function(o){ return o.obj.price/o.obj.cps() } ) );
-        target.obj = maxs.obj;
+    if($g.cookiesPs<0.1){
+      target = {
+        type: 'obj',
+        obj: $o[0],
+        s: 0,
       }
+    } else {
+      var scores = [];
+      this.getScoresForUpgrade(scores);
+      this.getScoresForObjects(scores);
+      target = Util.maxBy( scores, function(s){return s.s} );
     }
 
     target.status = this.status;
@@ -260,58 +267,293 @@ var Cookiesmith = (function($g,$app){
     this.choice = target;
     this.action = this.buy;
 
-    if(target.obj){
-      var delay = target.obj.price<$g.cookies ? 0 : (target.obj.price-$g.cookies)/this.realCps;
-      Util.log('plan to buy a '+target.obj.name+' at '+Beautify(target.obj.price)+' within '+Math.round(delay)+' seconds' );
+    if(target.type==='obj'){
+      var delay = target.obj.price<$g.cookies ? 0 : Math.ceil((target.obj.price-$g.cookies)/this.realCps);
+      if(delay===0){
+        return this.buy();
+      } else {
+        Util.log('plan to buy a '+target.obj.name+' at '+Beautify(target.obj.price)+' after '+delay+' seconds' );
+      }
 
-    } else if (target.ug){
-      var delay = target.ug.basePrice<$g.cookies ? 0 : (target.ug.basePrice-$g.cookies)/this.realCps;
-      Util.log('plan to buy the '+target.ug.name+' at '+Beautify(target.ug.basePrice)+' within '+Math.round(delay)+' seconds' );
-
+    } else if (target.type==='ug'){
+      var delay = target.ug.basePrice<$g.cookies ? 0 : Math.ceil((target.ug.basePrice-$g.cookies)/this.realCps);
+      if(delay===0){
+        return this.buy();
+      } else {
+        Util.log('plan to buy the '+target.ug.name+' at '+Beautify(target.ug.basePrice)+' after '+delay+' seconds' );
+      }
     }
 
   };
-  SimpleBuyer.prototype.considerUpgrade = function(){
-    var maxPriceObj = Util.maxBy( $o, function(o){ return o.bought>0 ? o.price : 0 } );
+  SimpleBuyer.prototype.getScoresForObjects = function(scores){
+    scores = scores || [];
+    for(var i=0;i<$o.length;i++){
+      var score = this.scoreForObject($o[i]);
+      if(score!==undefined)
+        scores.push( { type:'obj', s: score, obj: $o[i] } );
+    }
+    return scores;
+  };
+  SimpleBuyer.prototype.getScoresForUpgrade = function(scores){
+    scores = scores||[];
     for(var i=0;i<$u.length;i++){
       var ug = $u[i];
       if(ug.bought===1 || ug.unlocked===0 ){ continue; }
 
-      var policy = this.upgradePolicies[ug.name];
-      if( policy !== undefined ){
-        if(policy.name === 'ignore'){
-          continue;
-        }
+      var policy = this.policiesForUpgrade[ug.name];
+
+      if (policy===null){
+        continue;
+      } else if( typeof policy !== 'function'){
+        policy = this.upgradePolicies.Default;
       }
 
-      if(maxPriceObj.price < 800000){
-        var r = 1.3;
-      } else if(maxPriceObj.price < 1666666) {
-        var r = 1.1;
-      } else if(maxPriceObj.price < 500000000){
-        var r = 0.8;
-      } else {
-        var r = 0.6;
-      }
-
-      if( ug.basePrice < maxPriceObj.price * r ){
-        return ug;
-      }
+      var score = policy(ug);
+      if(score!==null)
+          scores.push({ type:'ug', s: score, ug: ug, });
     }
-    return undefined;
+    return scores;
   };
-  SimpleBuyer.prototype.upgradePolicies = {'One mind':{name:'ignore'}};
-  SimpleBuyer.prototype.score = function(obj){
-    var cpcps = obj.price / obj.cps();
-    var delay = obj.price > $g.cookies ? (obj.price-$g.cookies)/this.realCps : 0;
+  SimpleBuyer.prototype.getPoliciesForUpgrade = function(){
+    var buyer = this;
+    function ugDelay(ug){
+      return ug.basePrice > $g.cookies ? (ug.basePrice-$g.cookies)/buyer.realCps : 0;
+    }
+    function gainGlobalCpsMult(rate){
+      return function(ug){
+        var cpcps =  ug.basePrice / ($g.cookiesPs/$g.globalCpsMult*rate);
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function gainBase(objName,base){
+      var obj = $O[objName];
+      return function(ug){
+        if(obj.amount===0) return null;
+        var cpcps = ug.basePrice / (base * obj.amount);
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function gainRate(objName,rate){
+      var obj = $O[objName];
+      return function(ug){
+        if(obj.amount===0) return null;
+        var cpcps = ug.basePrice / (obj.storedCps * obj.amount * (rate-1));
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    };
+    function twice(objName){
+      return gainRate(objName,2);
+    }
+    function gainMouseAndCursorBase(mouseBase,cursorBase){
+      return function(ug){
+        var cps = (mouseBase * buyer.clicksPs) + (cursorBase * $o[0].amount);
+        var cpcps = ug.basePrice / cps;
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function twiceMouseAndCursor(){
+      return function(ug){
+        var cps = $o[0].storedCps + buyer.clickCps;
+        var cpcps = ug.basePrice / cps;
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function gainMouseAndCursorByNonCursor(base){
+      return function(ug){
+        var amount = 0;
+        for(var i=1;i<$o.length;i++){
+          amount += $o[i].amount;
+        }
+        var cpcps = ug.basePrice / ( base * (amount + buyer.clicksPs));
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function gainClickByCps(rate){
+      return function(ug){
+        var cpcps = ug.basePrice / ($g.cookiesPs * rate * buyer.clicksPs);
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function kitten(rate){
+      return function(ug){
+        var cpcps = ug.basePrice / ($g.cookiesPs * ($g.milkProgress*rate));
+        return -buyer.cost(cpcps,ugDelay(ug));
+      };
+    }
+    function delayLessThan(threshold){
+      return function(ug){
+        return ugDelay(ug)>threshold ? null : 0;
+      };
+    }
+
+    var LuckyTwice = delayLessThan( buyer.param.luckyCookiesThreshold );
+
+    var BuySoon = function(){return Infinity}
+    var Default = delayLessThan( buyer.param.upgradeDefaultThreshold );
+    var Ignore = null;
 
     return {
-      s: - this.cost(cpcps,delay),
-      obj: obj,
+      // used when no policies matched
+      Default: Default,
+
+      // Cursor
+      'Reinforced index finger': gainMouseAndCursorBase(1,0.1),
+      'Carpal tunnel prevention cream': twiceMouseAndCursor(),
+      'Ambidextrous': twiceMouseAndCursor(),
+      'Thousand fingers': gainMouseAndCursorByNonCursor(0.1),
+      'Million fingers': gainMouseAndCursorByNonCursor(0.5),
+      'Billion fingers': gainMouseAndCursorByNonCursor(2),
+      'Trillion fingers': gainMouseAndCursorByNonCursor(10),
+      'Quadrillion fingers': gainMouseAndCursorByNonCursor(20),
+      'Quintillion fingers': gainMouseAndCursorByNonCursor(100),
+
+      // Mouse
+      'Plastic mouse': gainClickByCps(0.01),
+      'Iron mouse': gainClickByCps(0.01),
+      'Titanium mouse': gainClickByCps(0.01),
+      'Adamantium mouse': gainClickByCps(0.01),
+
+      // Grandma
+      'Forwards from grandma': gainBase('Grandma',0.3),
+      'Steel-plated rolling pins': twice('Grandma'),
+      'Lubricated dentures': twice('Grandma'),
+      'Prune juice': twice('Grandma'),
+      'Farmer grandmas': twice('Grandma'),
+      'Worker grandmas': twice('Grandma'),
+      'Miner grandmas': twice('Grandma'),
+      'Cosmic grandmas': twice('Grandma'),
+      'Transmuted grandmas': twice('Grandma'),
+      'Altered grandmas': twice('Grandma'),
+      "Grandmas' grandmas": twice('Grandma'),
+      "Antigrandmas": twice('Grandma'),
+
+      // Farm
+      'Cheap hoes': gainBase('Farm',0.5),
+      'Fertilizer': twice('Farm'),
+      'Cookie trees': twice('Farm'),
+      'Genetically-modified cookies': twice('Farm'),
+
+      // Factory
+      'Sturdier conveyor belts': gainBase('Factory',4),
+      'Child labor': twice('Factory'),
+      'Sweatshop': twice('Factory'),
+      'Radium reactors': twice('Factory'),
+
+      // Mine
+      'Sugar gas': gainBase('Mine',10),
+      'Megadrill': twice('Mine'),
+      'Ultradrill': twice('Mine'),
+      'Ultimadrill': twice('Mine'),
+
+      // Shipment
+      'Vanilla nebulae': gainBase('Shipment',30),
+      'Wormholes': twice('Shipment'),
+      'Frequent flyer': twice('Shipment'),
+      'Warp drive': twice('Shipment'),
+
+      // Alchemy lab
+      'Antimony': gainBase('Alchemy lab',100),
+      'Essence of dough': twice('Alchemy lab'),
+      'True chocolate': twice('Alchemy lab'),
+      'Ambrosia': twice('Alchemy lab'),
+
+      // Portal
+      'Ancient tablet': gainBase('Portal',1666),
+      'Insane oatling workers': twice('Portal'),
+      'Soul bond': twice('Portal'),
+      'Sanity dance': twice('Portal'),
+
+      // Time machine
+      'Flux capacitors': gainBase('Time machine',9874),
+      'Time paradox resolver': twice('Time machine'),
+      'Quantum conundrum': twice('Time machine'),
+      'Causality enforcer': twice('Time machine'),
+
+      // Antimatter condenser
+      'Sugar bosons': gainBase('Antimatter condenser',99999),
+      'String theory': twice('Antimatter condenser'),
+      'Large macaron collider': twice('Antimatter condenser'),
+      'Big bang bake': twice('Antimatter condenser'),
+
+      // Golden Cookies
+      'Lucky day': LuckyTwice,
+      'Serendipity': LuckyTwice,
+      'Get lucky': LuckyTwice,
+
+      // Kittens
+      'Kitten helpers': kitten(0.05),
+      'Kitten workers': kitten(0.1),
+      'Kitten engineers': kitten(0.2),
+      'Kitten overseers': kitten(0.3),
+
+      // Cookies
+      'Oatmeal raisin cookies': gainGlobalCpsMult(0.05),
+      'Peanut butter cookies': gainGlobalCpsMult(0.05),
+      'Plain cookies': gainGlobalCpsMult(0.05),
+      'Coconut cookies': gainGlobalCpsMult(0.05),
+      'White chocolate cookies': gainGlobalCpsMult(0.05),
+      'Macadamia nut cookies': gainGlobalCpsMult(0.05),
+      'Sugar cookies': gainGlobalCpsMult(0.05),
+
+      'Double-chip cookies': gainGlobalCpsMult(0.1),
+      'White chocolate macadamia nut cookies': gainGlobalCpsMult(0.1),
+      'All-chocolate cookies': gainGlobalCpsMult(0.1),
+
+      'Dark chocolate-coated cookies': gainGlobalCpsMult(0.15),
+      'White chocolate-coated cookies': gainGlobalCpsMult(0.15),
+      'Eclipse cookies': gainGlobalCpsMult(0.15),
+      'Zebra cookies': gainGlobalCpsMult(0.15),
+      'Snickerdoodles': gainGlobalCpsMult(0.15),
+      'Stroopwafels': gainGlobalCpsMult(0.15),
+      'Macaroons': gainGlobalCpsMult(0.15),
+      'Empire biscuits': gainGlobalCpsMult(0.15),
+      'British tea biscuits': gainGlobalCpsMult(0.15),
+      'Chocolate british tea biscuits': gainGlobalCpsMult(0.15),
+      'Round british tea biscuits': gainGlobalCpsMult(0.15),
+      'Round chocolate british tea biscuits': gainGlobalCpsMult(0.15),
+      'Round british tea biscuits with heart motif': gainGlobalCpsMult(0.15),
+      'Round chocolate british tea biscuits with heart motif': gainGlobalCpsMult(0.15),
+
+      'Madeleines': gainGlobalCpsMult(0.2),
+      'Palmiers': gainGlobalCpsMult(0.2),
+      'Palets': gainGlobalCpsMult(0.2),
+      'Sabl&eacute;s': gainGlobalCpsMult(0.2),
+
+      // Research
+      'Bingo center/Research facility': gainRate('Grandma',4),
+      'Specialized chocolate chips': gainGlobalCpsMult(0.01),
+      'Designer cocoa beans': gainGlobalCpsMult(0.02),
+      'Ritual rolling pins': twice('Grandma'),
+      'Underworld ovens': gainGlobalCpsMult(0.03),
+
+      // Research(after One mind)
+      'Exotic nuts': gainGlobalCpsMult(0.04),
+      'Arcane sugar': gainGlobalCpsMult(0.05),
+      'Sacrificial rolling pins': Ignore,
+
+      // door for wrath
+      'One mind': Ignore,
+      'Communal brainsweep': Ignore,
+      'Elder Pact': Ignore,
+
+      // Repeatable
+      'Elder Pledge': Ignore,
+      'Elder Covenant': Ignore,
+      'Revoke Elder Covenant': Ignore,
+
+      // "Debug purpose only"
+      'Ultrascience': Ignore,
+      'Gold hoard': Ignore,
+      'Neuromancy': Ignore,
     };
+  };
+  SimpleBuyer.prototype.scoreForObject = function(obj){
+    var cpcps = obj.price / obj.storedCps;
+    var delay = obj.price > $g.cookies ? (obj.price-$g.cookies)/this.realCps : 0;
+    return - this.cost(cpcps,delay);
   }
   SimpleBuyer.prototype.cost = function(cpcps,delay){
-    return cpcps * (1+Math.pow(delay/this.costParam,2));
+    return cpcps * (1+Math.pow(delay/this.param.costDenom,2));
   };
   SimpleBuyer.prototype.status = function(){
     var stat = $g.cookiesPs;
@@ -347,7 +589,11 @@ var Cookiesmith = (function($g,$app){
   Cheater.iAmNotACheater = function(){
     $g.Achievements['Cheated cookies taste awful'].won = 0;
   };
-
+  Cheater.getNeuromancy = function(){
+    var neuro = $U['Neuromancy'];
+    this.getCookies(neuro.basePrice);
+    neuro.buy();
+  }
 
   /*
    * initializer
@@ -368,6 +614,7 @@ var Cookiesmith = (function($g,$app){
     GoldHunter.start();
     Buyer.start();
     started = true;
+    Util.log('operation started');
     return $app;
   };
   $app.remove = function(){
